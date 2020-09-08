2a74ec27e6658cf6632742a27c22bfeab8d06922
LinusRonnbladIT19A <70194416+LinusRonnbladIT19A@users.noreply.github.com>
2020-09-08 09:40:32 +0200

    8/9

---
+++ Programmering 1/Index.js
@@ -0,0 +1,45 @@
+let name = "Linus"
+let age = 18
+
+console.log (`Hej ${name} du är ${age}år gammal`)
+
+if (age >=18){console.log("Du är vuxen")
+ } else  {console.log ("du är barn")}
+
+let text = document.querySelector("#text")
+text.innerHTML =`${name} är ${age}år gammal`
+
+let mult = document.querySelector("#multiplikation")
+let div = document.querySelector("#division")
+let add = document.querySelector("#addition")
+let sub = document.querySelector("#subtraktion")
+
+mult.innerHTML =`Multiplikation`
+div.innerHTML =`division`
+add.innerHTML =`addition`
+sub.innerHTML =`subtraktion`
+
+let button = document.querySelector("#knapp")
+
+button.addEventListener("click",Math)
+
+function Math (event){
+console-log ("math-funktionen körs")
+let num1 = parseFloat(document.querySelector("number1").value)
+let num2 = parseFloat(document.querySelector("number2").value)
+
+
+add.innerHTML = `${num1}+${num2}=${num1+num2} `
+sub.innerHTML = `${num1}-${num2}=${num1-num2} `
+mult.innerHTML = `${num1}*${num2}=${num1*num2} `
+div.innerHTML = `${num1}/${num2}=${num1/num2} `
+
+
+}
+
+
+
+
+
+
+
