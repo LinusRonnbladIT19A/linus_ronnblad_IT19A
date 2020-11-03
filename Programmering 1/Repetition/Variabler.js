4a8200d226afabda989832aeed575f294270d5b8
LinusRonnbladIT19A <70194416+LinusRonnbladIT19A@users.noreply.github.com>
2020-11-03 09:36:15 +0100

    2020 11 03

---
+++ Programmering 1/Repetition/Variabler.js
@@ -0,0 +1,14 @@
+// Skapar variabel och ger den värdet Linus
+let name="Linus"
+let age = 17
+
+// Skriver ut i consol
+console.log ( `Hej ${name}` )
+console.log (  `Jag är ${age} år Gammal `    )
+
+if (age >= 18 ) { 
+    console.log ("DU är vuxen")
+} else {
+    console.log ("DU är Barn")
+}
+
