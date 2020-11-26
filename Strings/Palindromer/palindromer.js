babc94753079b1d7fa97e56a1025868dd9fb6b4a
LinusRonnbladIT19A <70194416+LinusRonnbladIT19A@users.noreply.github.com>
2020-11-26 10:02:17 +0100

    2020 11 26

--- Strings/Palindromer/palindromer.js
+++ Strings/Palindromer/palindromer.js
@@ -1,2 +1,26 @@
-let = document.querySelector("#textArea")
+let textArea = document.querySelector("#textArea")
+let knapp = document.querySelector("#knapp")
+let p_diplaywords = document.querySelector("#displaywords")
+ 
+knapp.addEventListener("click", countWords);
 
+function countWords(){
+
+  if (palindrome(textArea.value)){
+    p_diplaywords.innerHTML = "Det är palindrom"
+  } else {
+
+    p_diplaywords.innerHTML = "Det är inte palindrom"
+  }
+
+}
+
+
+function palindrome(str){
+    let reversed = str.split("").reverse().join("")
+    console.log(reversed)
+    if (reversed === str) 
+      return true;
+
+    return false;
+}
