ee25b634fb57dfc642c9852d1646909187520fc5
LinusRonnbladIT19A <70194416+LinusRonnbladIT19A@users.noreply.github.com>
2022-05-24 10:49:47 +0200

    2022 05 24

--- Strings/Palindromer/palindromer.js
+++ Strings/Palindromer/palindromer.js
@@ -1,11 +1,13 @@
 let textArea = document.querySelector("#textArea")
 let knapp = document.querySelector("#knapp")
 let p_diplaywords = document.querySelector("#displaywords")
- 
+let kopieras = document.querySelector("#kopieras")
+let test = textArea.value
+ console.log(test)
 knapp.addEventListener("click", countWords);
 
 function countWords(){
-
+  kopieras.innerHTML = `Du har angivit texten: ${test}  `
   if (palindrome(textArea.value)){
     p_diplaywords.innerHTML = "Det är palindrom"
   } else {
